b92ea7a75b232e6f89b53e1a691f028b9b68a0f5
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-11-11 15:50:44 -0500

    Adjust layout height for improved responsiveness in LayoutWrapper

--- src/app/(components)/(layouts)/LayoutWrapper.tsx
+++ src/app/(components)/(layouts)/LayoutWrapper.tsx
@@ -4,15 +4,14 @@ import Header from "@/app/(components)/(layouts)/Header"
 
 const LayoutWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    // grid items-center justify-items-center min-h-screen 
     <div className="
-      min-h-screen bg-stone-100
+      min-h-[calc(100vh-4rem)] bg-stone-100
       font-[family-name:var(--font-geist-sans)]
     ">
       <Header />
 
       <div className="
-        min-h-[calc(100vh-7rem)] mt-16 p-6
+        mt-16 p-6
         md:p-12 lg:px-80 2xl:px-96
       ">{ children }</div>
 
