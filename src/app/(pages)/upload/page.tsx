e5db326b98244229a91408afef184e876546d1cf
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-11-11 18:16:11 -0500

    Add PasswordForm component for password verification on upload page

--- src/app/(pages)/upload/page.tsx
+++ src/app/(pages)/upload/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState, FormEvent } from "react";
+import { useState } from "react";
 import Image from "next/image";
+import PasswordForm from "@/app/(components)/passwordForm";
 
 const UploadPage = () => {
   const router = useRouter();
 
-  const [passwordState, setPasswordState] = useState({
-    hasAccess: false,
-    pwd: "",
-    warningMessage: ""
-  });
+  const [hasAccess, setHasAccess] = useState(false);
   const [formData, setFormData] = useState({
     title: "",
     thumbnailImage: null as File | null,
@@ -22,30 +19,6 @@ const UploadPage = () => {
   const [thumbnailImageUrl, setThumbnailImageUrl] = useState<string | null>(null);
   const [thumbnailFeedback, setThumbnailFeedback] = useState("");
   const [photosFeedback, setPhotosFeedback] = useState("");
-
-  const handlePasswordSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    
-    try {
-      const res = await fetch('/api/verifyPassword', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password: passwordState.pwd }),
-      });
-
-      const result = await res.json();
-
-      if (result.success) {
-        setPasswordState({ ...passwordState, hasAccess: true })
-      } else {
-        setPasswordState({ ...passwordState, pwd: "", warningMessage: "Incorrect password" })
-      }
-    } catch (error) {
-      console.error("Error verifying password", error)
-    }
-  }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -128,24 +101,7 @@ const UploadPage = () => {
     return feedback.startsWith('NOPE') ? 'text-red-500' : 'text-green-500';
   };
 
-  return !passwordState.hasAccess ? (
-    // Password form
-    <div className="grid justify-items-center h-[calc(100vh-10rem)]">
-      <form onSubmit={handlePasswordSubmit} className="w-64 flex flex-col gap-4 justify-center items-center">
-      <input
-        type="password"
-        value={passwordState.pwd}
-        placeholder="Password"
-        onChange={e => setPasswordState({ ...passwordState, pwd: e.target.value, warningMessage: "" })}
-        className="w-full p-2 border border-gray-300 rounded-md outline-none border-none"
-      />
-      <button type="submit" className="w-full p-2 bg-stone-500 text-white rounded-md font-medium">
-        Submit
-      </button>
-      <div className="h-4 mt-2 px-1 text-sm text-red-600">{passwordState.warningMessage}</div>
-      </form>
-    </div>
-  ) : (
+  return !hasAccess ? <PasswordForm hasAccess={() => setHasAccess(true)} /> : (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md space-y-6 w-full max-w-lg">
         <h2 className="text-2xl font-bold text-center text-gray-700">Create A New Album</h2>
